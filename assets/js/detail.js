51193bc6f8935e16669970e39dfbfcbcb5c45844
lsw725 <725lsw@hanmail.net>
2020-12-04 02:45:46 +0900

    Design Menu, Detail Page ver2.2

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -7,5 +7,28 @@
             $(this).parent().prevAll().css({"color": "#f8b600"});
             $(this).parent().nextAll().css({"color": "gray"});
         });
+
+        $("#temp").on("click", function(){
+            if($("[name='nickname']").val() == '' || $("[name='order-date']").val() == undefined) {
+                $('#comment-modal').modal();
+            } 
+        });
+
+        $("form").submit(function(){
+            if($("[name='nickname']").val() == '' ||
+                    $("[name='order-date']").val() == undefined ||
+                            $("#comment-body").val() == '') {
+                $('#comment-modal').modal();
+                return false
+            }
+            return true
+        });
+
+        
     });
 })(jQuery);
+
+function check(form) {
+    alert(form.name.val())
+    
+}
