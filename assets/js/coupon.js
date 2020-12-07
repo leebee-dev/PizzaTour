ee7c5783a6f78e441968cdf46198536211559be8
suyeon <ssy4230@naver.com>
2020-12-07 22:46:54 +0900

    QR real complete

--- assets/js/coupon.js
+++ assets/js/coupon.js
@@ -15,7 +15,7 @@ showCupon(cupon);
     jQuery(document).ready(function() {
         var qrcode = null;
         var id = "";
-        var count = 0;
+        var count = cupon;
 
         $("#qrcode span").text("사용하기를 누르시면 QRcode가 생성됩니다.");
         new QRCode(document.getElementById("qrcode"), {
@@ -29,27 +29,23 @@ showCupon(cupon);
 
         $("#qrcode").children("img").addClass("empty-qrcode");
 
-        $("#saving-btn").on("click", function(){
+        /*$("#saving-btn").on("click", function(){
             count++;
             if(qrcode == null) qrcode = createQRCode(count);
-        });
+        });*/
 
         $("#using-btn").on("click", function(){
-            $('.modal-title').text("직원확인");
+            if(count >= 12) {
+                $('.modal-title').text("직원확인");
                 $('.modal-body p').text("직원확인 처리하시겠습니까? \n 직원 확인 후에는 쿠폰은 사용처리됩니다.");
                 $('#info-modal').modal();
                 location.href = "delete_coupon.php";
-            /*if(count >= 12) {
-                count -= 12;
-                $('.modal-title').text("사용 불가");
-                $('.modal-body p').text(`아직 ${12-count}개 덜 모으셨어요~! 조금만 더 파이팅`);
-                $('#info-modal').modal();
-                delete_coupon();
             } else {
                 $('.modal-title').text("사용 불가");
                 $('.modal-body p').text(`아직 ${12-count}개 덜 모으셨어요~! 조금만 더 파이팅`);
                 $('#info-modal').modal();
-            }*/
+                location.reload();
+            }
         });
     });
 })(jQuery);
